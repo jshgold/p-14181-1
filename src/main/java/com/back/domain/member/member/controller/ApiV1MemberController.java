f9d1e9f8a64ee7dd2f5a33ca6b0b88311c079b9c
devho <jshgold93@gmail.com>
2026-06-16 10:07:52 +0900

    35강 수업내용

---
+++ src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
@@ -0,0 +1,95 @@
+package com.back.domain.member.member.controller;
+
+
+import com.back.domain.member.member.dto.MemberDto;
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.member.member.service.MemberService;
+import com.back.global.exception.ServiceException;
+import com.back.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
+@Tag(name = "ApiV1MemberController", description = "API 회원 컨트롤러")
+public class ApiV1MemberController {
+    private final MemberService memberService;
+
+
+    public record MemberJoinReqBody(
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String username,
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String password,
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String nickname
+    ) {
+    }
+
+    @PostMapping
+    @Transactional
+    public RsData<MemberDto> join(
+            @RequestBody @Valid MemberJoinReqBody reqBody
+    ) {
+        Member member = memberService.join(
+                reqBody.username(),
+                reqBody.password(),
+                reqBody.nickname()
+        );
+
+        return new RsData<>(
+                "201-1",
+                "%s님 환영합니다. 회원가입이 완료되었습니다.".formatted(member.getName()),
+                new MemberDto(member)
+        );
+    }
+
+
+    public record MemberLoginReqBody(
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String username,
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String password
+    ) {
+    }
+
+    public record MemberLoginResBody(
+            MemberDto item,
+            String apiKey
+    ) {
+    }
+
+    @PostMapping("/login")
+    public RsData<MemberLoginResBody> login(
+            @RequestBody @Valid MemberLoginReqBody reqBody
+    ) {
+        Member member = memberService.findByUsername(reqBody.username())
+                .orElseThrow(() -> new ServiceException("401-1", "존재하지 않는 아이디입니다."));
+
+        if (!member.getPassword().equals(reqBody.password()))
+            throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
+
+        return new RsData<>(
+                "200-1",
+                "%s님 환영합니다.".formatted(member.getName()),
+                new MemberLoginResBody(
+                        new MemberDto(member),
+                        member.getApiKey()
+                )
+        );
+    }
+}
