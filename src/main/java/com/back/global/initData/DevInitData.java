c369dc96aa4ebdc32883a9a4ba16abd0315e6be2
devho <jshgold93@gmail.com>
2026-06-24 10:57:27 +0900

    백엔드 재기동시 변경사항 스키마 재생성

--- src/main/java/com/back/global/initData/DevInitData.java
+++ src/main/java/com/back/global/initData/DevInitData.java
@@ -1,10 +1,14 @@
 package com.back.global.initData;
 
+import com.back.standard.util.Ut;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @Profile("dev")
 @Configuration
@@ -13,7 +17,14 @@ public class DevInitData {
     @Bean
     ApplicationRunner devInitDataApplicationRunner() {
         return args -> {
-            log.debug("개발 환경에서 초기 데이터가 설정되었습니다!");
+            Ut.cmd.runAsync(
+                    "npx{{DOT_CMD}}",
+                    "--yes",
+                    "--package", "typescript@v5",
+                    "--package", "openapi-typescript",
+                    "openapi-typescript", "http://localhost:8080/v3/api-docs/apiV1",
+                    "-o", "../front/src/lib/backend/apiV1/schema.d.ts"
+            );
         };
     }
 }
